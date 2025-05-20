84b503cf7b4b90eba90c272a9482a366cb57af9a
MuhamadFathir <fathirrizqii866@gmail.com>
2025-05-20 09:45:04 +0700

    done

--- c19/server.js
+++ c19/server.js
@@ -1,35 +1,51 @@
 const http = require('http')
+
 const fs = require('fs')
+
 const index = fs.readFileSync('index.html', 'utf-8')
+
+const add = fs.readFileSync('add.html', 'utf-8')
+
 const data = fs.readFileSync('data.json', 'utf-8')
 
 http.createServer(function (req, res) {
-    res.writeHead(200, { "Content-type": "text/html" })
-    fs.readFile('data.json', 'utf-8', (err, data) => {
-        if (err) return res.end('gagal')
+	res.writeHead(200, { "Content-type": "text/html" })
 
-        let html = ''
-        const value = JSON.parse(data)
+	switch (req.url) {
+		case '/':
+			fs.readFile('data.json', 'utf-8', (err, data) => {
+				if (err) return res.end('gagal')
 
-        value.forEach((item, index) => {
-            html += `
-            <tr>
-                <td>${index + 1}</td>
-                <td>${item.name}</td>
-                <td>${item.height}</td>
-                <td>${item.weight}</td>
-                <td>${item.birthdate}</td>
-                <td>${item.married}</td>
-                <td>
-                    <a href="">edit</a>
-                    <a href="">delete</a>
-                </td>
-            </tr>
-            
-            `
-        });
+				let html = ''
+				const value = JSON.parse(data)
 
-        let result = index.replace('{table content}', html)
-        res.end(result)
-    })
+				value.forEach((item, index) => {
+					html += `
+                    <tr>
+                        <td>${index + 1}</td>
+                        <td>${item.name}</td>
+                        <td>${item.height}</td>
+                        <td>${item.weight}</td>
+                        <td>${item.birthdate}</td>
+                        <td>${item.married}</td>
+                        <td>
+                            <a href="">edit</a>
+                            <a href="">delete</a>
+                        </td>
+                    </tr>
+                    
+                    `
+				});
+
+				let result = index.replace('{table content}', html)
+				res.end(result)
+			})
+			break;
+		case '/add':
+			res.end(add)
+			break;
+		default:
+			res.end('404 error')
+	}
+
 }).listen(3000);
